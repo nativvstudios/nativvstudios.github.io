1b5a6394589e9d2503642fe10093e03c33934fd8
Landin <landinowens@gmail.com>
2022-03-01 00:52:22 -0400

    Changes: -Added portfolio section -Added portfolio variables to config for less markdown

--- assets/js/lbox.js
+++ assets/js/lbox.js
@@ -3,10 +3,14 @@ lightbox.id = "lightbox";
 document.body.appendChild(lightbox);
 
 const images = document.querySelectorAll(".article .content img");
+const portImages = document.querySelectorAll(".items .item img");
+
 images.forEach((image) => {
   image.addEventListener("click", (e) => {
+    disableScroll();
     lightbox.classList.add("active");
     const img = document.createElement("img");
+    
     img.src = image.src;
     while (lightbox.firstChild) {
       lightbox.removeChild(lightbox.firstChild);
@@ -17,7 +21,78 @@ images.forEach((image) => {
   });
 });
 
+
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+
+
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+
+portImages.forEach((image) => {
+  image.addEventListener("click", (e) => {
+    disableScroll();
+    lightbox.classList.add("active");
+    const img = document.createElement("img");
+    img.src = image.src;
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild);
+    }
+    img.style.setProperty("cursor", "not-allowed");
+    lightbox.appendChild(img);
+    lightbox.style.setProperty("cursor", "zoom-out");
+    const closeButton = document.createElement("button");
+
+    });
+});
+
+
 lightbox.addEventListener("click", (e) => {
   if (e.target !== e.currentTarget) return;
   lightbox.classList.remove("active");
+  enableScroll();
+});
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target !== e.currentTarget) return;
+  lightbox.classList.remove("active");
+  enableScroll();
 });
