aa93e84b6f801461b0c4e3ec8805656c94ccbf2e
nativvstudios <landinowens@gmail.com>
2022-12-08 18:48:22 -0400

    Changes: -Updated the life page -Added our favourite podcasts to top of life page

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,7 +1,32 @@
 ---
 ---
 
-const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
+//Grabs the categories from the data file and creates a list of them
+const categories = {
+  {% for category in site.categories %}
+  {% capture category_name %}{{ category | first }}{% endcapture %}
+  {{ category_name }}: [
+    {% for post in site.categories[category_name] %}
+    {
+      url: `{{ site.baseurl }}{{ post.url }}`,
+      date: `{{post.date | date_to_string}}`,
+      title: `{{post.title}}`
+    },
+    {% endfor %}
+  ],
+  {% endfor %}
+};
+
+//Grabs the artists from the data file and creates a list of them
+// const artists = {
+//   {% for i in site.data.author.artists %}
+//   {{ i.name }}: {
+//     url: `{{ i.url }}`,
+//     about: `{{i.about}}`,
+//     title: `{{i.name}}`
+//   },
+//   {% endfor %}
+// };
 
 window.onload = function () {
   document.querySelectorAll(".category").forEach((category) => {
@@ -10,7 +35,7 @@ window.onload = function () {
       let html = ``
       posts.forEach(post=>{
         html += `
-        <a class="modal-article" href="${post.url}">
+        <a class="modal-article" href="${post.url}">  
           <h4>${post.title}</h4>
           <small class="modal-article-date">${post.date}</small>
         </a>
